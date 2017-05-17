9c537bf41f9c8230c6ceb89226f8d03b84724715
itwill <itwill@itwillbs02-08>
2017-05-17 15:56:58 +0900

    PackContent.java 수정

--- TeamK/src/net/pack/action/PackContent.java
+++ TeamK/src/net/pack/action/PackContent.java
@@ -1,11 +1,15 @@
 package net.pack.action;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import net.pack.db.PackBean;
 import net.pack.db.PackDAO;
+import net.reply.db.ReplyBean;
+import net.reply.db.ReplyDAO;
 
 public class PackContent implements Action{
 
@@ -16,12 +20,14 @@ public class PackContent implements Action{
 		HttpSession session = request.getSession();
 		
 		int num = Integer.parseInt(request.getParameter("num"));
-		String pageNum = request.getParameter("pageNum");
+		String repageNum = request.getParameter("repageNum");
 		PackDAO pdao = new PackDAO();
 		PackBean pb = new PackBean();
+		ReplyDAO rdao = new ReplyDAO();
+		ReplyBean rb = new ReplyBean();
 		
 		System.out.println("PackContent num >> " + num);
-		System.out.println("PackContent pagenum >> " + pageNum);
+		System.out.println("PackContent pagenum >> " + repageNum);
 		
 		pb = pdao.getPack(num);
 		pdao.updateReadcount(num);
@@ -30,6 +36,60 @@ public class PackContent implements Action{
 		System.out.println("Sarea >> " + pb.getSarea());
 		
 		session.setAttribute("PackBean", pb);
+		
+		
+		//전체글 횟수 구하기 int count = getBoardCount()
+		int count = rdao.getCommentCount(num);//pdao.getBoardCount();
+		
+		//한페이지에 보여줄 글의 갯수
+		int pagesize = 5;
+		//시작행 구하기   1,  11,  21,  31,  41  ...... 
+		
+		//현재페이지가 몇페이지인지 가져오기
+		if(repageNum == null)
+			repageNum = "1";
+		
+		int currentPage = Integer.parseInt(repageNum);
+		int startRow = (currentPage-1)*pagesize+1;
+		
+		
+		//끝행 구하기
+		int endRow = currentPage*pagesize;	
+		// 페이지 갯수 구하기
+		int pageCount = count/pagesize + (count%pagesize == 0 ? 0 : 1);
+		//한 화면에 보여줄 페이지 번호 갯수
+		int pageBlock = 10;
+		// 시작 페이지 구하기
+		int startPage = ((currentPage-1)/pageBlock)*pageBlock+1;
+		// 끝페이지 구하기
+		int endPage = startPage+pageBlock-1;
+
+		List replylist = rdao.getCommentList(startRow, pagesize, num);
+		
+		request.setAttribute("replylist", replylist);
+		
+		request.setAttribute("count", count);
+		request.setAttribute("repageNum", repageNum);
+		request.setAttribute("pageCount", pageCount);
+		request.setAttribute("pageBlock", pageBlock);
+		request.setAttribute("startPage", startPage);
+		request.setAttribute("endPage", endPage);
+		request.setAttribute("currentPage", currentPage);
+		request.setAttribute("pagesize", pagesize);
+		
+		
+		System.out.println("count >> " + count);
+		System.out.println("repageNum >> " + repageNum);
+		System.out.println("pageCount >> " + pageCount);
+		System.out.println("pageBlock >> " + pageBlock);
+		System.out.println("startPage >> " + startPage);
+		System.out.println("endPage >> " + endPage);
+		System.out.println("currentPage >> " + currentPage);
+		System.out.println("pagesize >> " + pagesize);
+				
+		
+		
+		
 		
 		
 		
