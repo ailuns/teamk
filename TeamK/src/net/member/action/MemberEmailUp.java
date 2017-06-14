2ba1cac21665d6ca3f693af8f67c1d8aafd31f5f
itwill <itwill@itwillbs02-08>
2017-06-14 18:05:47 +0900

    MemberEmailUp 수정완료

--- TeamK/src/net/member/action/MemberEmailUp.java
+++ TeamK/src/net/member/action/MemberEmailUp.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 import javax.mail.Address;
 import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -38,38 +39,70 @@ public class MemberEmailUp implements Action {
 			}
 			checknum = buf.toString();
 
-			String sender = "itwillbs8@itwillbs8.cafe24.com";
-			String receiver = email;
-			String subject = "[Team K 여행사]인증 번호";
+			final String subject = "[Team K 여행사]인증 번호";
 
-			String content = "인증번호 : " + checknum ;
+			final String content = "인증번호 : " + checknum ;
 
-			String server = "smtp.cafe24.com";
+			final String id = "itwillbs8@itwillbs8.cafe24.com";
+			final String pass = "itwillbs8030909";
+			int port = 587;
+			String host = "smtp.cafe24.com";
+			String from = "admin@itwillbs6.cafe24.com";
 
 			try {
-				Properties properties = new Properties();
-				properties.put("mail.smtp.host", server);
-				Session s = Session.getDefaultInstance(properties, null);
-				Message message = new MimeMessage(s);
+				Properties props = new Properties();
+				props.put("mail.stmp.starttls.enable", "true");
+				props.put("mail.smtp.auth", "true");
+				props.put("mail.smtp.host", host);
+		  		props.put("mail.smtp.port", port);
 
-				Address sender_address = new InternetAddress(sender);
-				Address receiver_address = new InternetAddress(receiver);
-
-				message.setHeader("content-type", "text/html;charset=utf-8");
-				message.setFrom(sender_address);
-				message.addRecipient(Message.RecipientType.TO, receiver_address);
+				Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(id, pass);
+					}
+				});
+				
+				session.setDebug(true);
+				Message message = new MimeMessage(session);
+				message.setFrom(new InternetAddress(from));
+				message.setRecipient(Message.RecipientType.TO, new InternetAddress(email));
+				
 				message.setSubject(subject);
-				message.setContent(content, "text/html;charset=utf-8");
-				message.setSentDate(new java.util.Date());
+				message.setContent(content, "text/html; charset=EUC-KR");
+				message.setText(content);
 
-				Transport transport = s.getTransport("smtp");
-				transport.connect(server, "itwillbs8", "itwillbs8030909");
-				transport.sendMessage(message, message.getAllRecipients());
-				transport.close();
+				Transport.send(message);
+					
+			} catch (Exception e) {e.printStackTrace();}
 
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+			
+			
+			
+			
+//			try {
+//				Properties properties = new Properties();
+//				properties.put("mail.smtp.host", server);
+//				Session s = Session.getDefaultInstance(properties, null);
+//				Message message = new MimeMessage(s);
+//
+//				Address sender_address = new InternetAddress(sender);
+//				Address receiver_address = new InternetAddress(receiver);
+//
+//				message.setHeader("content-type", "text/html;charset=utf-8");
+//				message.setFrom(sender_address);
+//				message.addRecipient(Message.RecipientType.TO, receiver_address);
+//				message.setSubject(subject);
+//				message.setContent(content, "text/html;charset=utf-8");
+//				message.setSentDate(new java.util.Date());
+//
+//				Transport transport = s.getTransport("smtp");
+//				transport.connect(server, "itwillbs8", "itwillbs8030909");
+//				transport.sendMessage(message, message.getAllRecipients());
+//				transport.close();
+//
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
 		}
 
 		request.setAttribute("checknum", checknum);
